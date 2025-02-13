595538436b64f3e22bd562404a5d842ad97b0ed9
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-02-13 12:32:34 +0100

    [backend/frontend] Clean

--- openbas-front/src/utils/api-types.d.ts
+++ openbas-front/src/utils/api-types.d.ts
@@ -291,7 +291,6 @@ export interface AttackPatternUpsertInput {
 }
 
 interface BasePayload {
-  expectationSignatureValue?: string;
   listened?: boolean;
   payload_arguments?: PayloadArgument[];
   payload_attack_patterns?: string[];
@@ -540,7 +539,6 @@ export interface ComcheckStatus {
 export interface Command {
   command_content: string;
   command_executor: string;
-  expectationSignatureValue?: string;
   listened?: boolean;
   payload_arguments?: PayloadArgument[];
   payload_attack_patterns?: string[];
@@ -623,7 +621,6 @@ export interface DirectInjectInput {
 
 export interface DnsResolution {
   dns_resolution_hostname: string;
-  expectationSignatureValue?: string;
   listened?: boolean;
   payload_arguments?: PayloadArgument[];
   payload_attack_patterns?: string[];
@@ -805,7 +802,6 @@ export interface EvaluationInput {
 
 export interface Executable {
   executable_file?: string;
-  expectationSignatureValue?: string;
   listened?: boolean;
   payload_arguments?: PayloadArgument[];
   payload_attack_patterns?: string[];
@@ -1151,7 +1147,6 @@ export interface ExportMapperInput {
 }
 
 export interface FileDrop {
-  expectationSignatureValue?: string;
   file_drop_file?: string;
   listened?: boolean;
   payload_arguments?: PayloadArgument[];
@@ -2194,7 +2189,6 @@ export interface MitigationUpsertInput {
 }
 
 export interface NetworkTraffic {
-  expectationSignatureValue?: string;
   listened?: boolean;
   network_traffic_ip_dst: string;
   network_traffic_ip_src: string;
